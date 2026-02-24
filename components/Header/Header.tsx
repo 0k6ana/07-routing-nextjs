d4d05f1ec745e2a462711480e52d4a440300f494
oksanA016 <frunzeoksana86@gmail.com>
2026-02-24 21:20:11 +0200

    fix header and default.css

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -9,13 +9,15 @@ const Header = () => {
       <Link href='/' aria-label='Home'>
         NoteHub
       </Link>
+
       <nav aria-label='Main Navigation'>
         <ul className={css.navigation}>
           <li>
             <Link href='/'>Home</Link>
           </li>
+
           <li>
-            <Link href='/notes'>Notes</Link>
+            <Link href='/notes/filter/all'>Notes</Link>
           </li>
         </ul>
       </nav>
